fa282036f21b7a07e5973c8929a7c2ccc12086f8
Youssef Akallal <youssef.akallal@gmail.com>
2020-12-31 18:31:10 -0500

    add inspectlet

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -61,6 +61,27 @@ const LayoutComponents = ({ children }) => {
         <html lang="en" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://www.google-analytics.com" />
+        <script type="text/javascript">
+          {`(function () {
+  window.__insp = window.__insp || [];
+  __insp.push(['wid', 229295937]);
+  var ldinsp = function () {
+    if (typeof window.__inspld != 'undefined') return;
+    window.__inspld = 1;
+    var insp = document.createElement('script');
+    insp.type = 'text/javascript';
+    insp.async = true;
+    insp.id = 'inspsync';
+    insp.src =
+      ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://cdn.inspectlet.com/inspectlet.js?wid=229295937&r=' +
+      Math.floor(new Date().getTime() / 3600000);
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(insp, x);
+  };
+  setTimeout(ldinsp, 0);
+})();`}
+        </script>
       </Helmet>
 
       <Flex flexDirection="column" style={{ minHeight: '100vh' }}>
